0b103fbf31a5367adf06383ee880408a0d5fca8b
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-03-28 12:54:57 -0700

    Concluido

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,97 @@ namespace Actividad_Integradora_1_Problema_3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(38, 88);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(188, 58);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(289, 88);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(188, 58);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(537, 88);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(188, 58);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(101, 251);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(37, 41);
+            this.lbl1.TabIndex = 3;
+            this.lbl1.Text = "A";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(371, 251);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(35, 41);
+            this.lbl2.TabIndex = 4;
+            this.lbl2.Text = "B";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(613, 251);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(37, 41);
+            this.lbl3.TabIndex = 5;
+            this.lbl3.Text = "C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Actividad Integradora 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,25 @@ namespace Actividad_Integradora_1_Problema_3
             InitializeComponent();
         }
 
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            lbl1.Text = "Sí";
+            lbl2.Text = "Sí";
+            lbl3.Text = "Sí";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            lbl1.Text = "No";
+            lbl2.Text = "No";
+            lbl3.Text = "No";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            lbl1.Text = "A";
+            lbl2.Text = "B";
+            lbl3.Text = "C";
+        }
     }
 }
